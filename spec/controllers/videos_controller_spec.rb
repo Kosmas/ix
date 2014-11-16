599e413fa3702f57f053c7a0fba19eeac05a5166
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-11-16 20:29:42 +0100

    add spec for rendering index view

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe VideosController, :type => :controller do
+  
+  describe 'GET index' do
 
+    it 'displays the index view' do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
 end
