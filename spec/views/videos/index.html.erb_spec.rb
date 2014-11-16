9b12a3ae91fdfed3307d9fab3411e984d15b5de4
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-11-16 20:40:39 +0100

    added spec and test for displaying index page with video titles

---
+++ spec/views/videos/index.html.erb_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe 'videos/index' do
+  let(:video_1) { Video.create!(title: 'First video') }
+  let(:video_2) { Video.create!(title: 'Second video') }
+
+  it 'renders the index page with correct dom elements' do
+    @videos = [video_1, video_2]
+    render
+    expect(rendered).to have_text(video_1.title)
+    expect(rendered).to have_text(video_2.title)
+  end
+end
