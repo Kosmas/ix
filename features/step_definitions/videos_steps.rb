d5c0f0147d92f99c3c0c659ee8244d33f01a9a53
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-11-19 21:23:05 +0100

    added more steps for new view and index scenarios

--- features/step_definitions/videos_steps.rb
+++ features/step_definitions/videos_steps.rb
@@ -9,3 +9,18 @@ end
 Then(/^I should see the video$/) do
   expect(page).to have_text(@video.title)
 end
+
+When(/^I select the video$/) do
+  click_on(@video.title)
+end
+
+Given(/^some videos$/) do
+  @first_video = Video.create(title: 'A Night on Earth')
+  @second_video = Video.create(title: 'Paris Texas')
+end
+
+Then(/^I should see the videos$/) do
+  expect(page).to have_text(@first_video.title)
+  expect(page).to have_text(@second_video.title)
+end
+
