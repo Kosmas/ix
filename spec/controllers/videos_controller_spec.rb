d8e4a55d22606b8b68d0ab2aece203eec1bb5a1f
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-11-19 21:25:17 +0100

    added spec for show action

--- spec/controllers/videos_controller_spec.rb
+++ spec/controllers/videos_controller_spec.rb
@@ -3,10 +3,18 @@ require 'rails_helper'
 RSpec.describe VideosController, :type => :controller do
   
   describe 'GET index' do
-
     it 'displays the index view' do
       get :index
       expect(response).to render_template(:index)
     end
+  end
+
+  describe 'GET show' do
+   let(:video) { Video.create(title: 'Very nice video') }
+
+   it 'displays the show view' do
+     get :show, id: video.id
+     expect(response).to render_template(:show)
+   end
   end
 end
