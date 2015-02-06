e8698b9ad6e9f0a885430568c04923d106afe7ff
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-02-06 14:00:02 +0100

    update schema file with new force: :cascade option

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20141206174240) do
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20141206174240) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
-  create_table "videos", force: true do |t|
+  create_table "videos", force: :cascade do |t|
     t.string   "title"
     t.string   "description"
     t.date     "release_date"
