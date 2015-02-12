78e2baa9a59e7e6fd7db0b015da4a7e921a2428e
Kosmas Chatzimichalis <kosmas@mach7x.com>
2015-02-13 00:39:30 +0100

    remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :videos
+  resources :videos, only: [:index, :show, :new, :create]
   root 'videos#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
